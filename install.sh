dd766c9658639eaa5cefe950357e8713416bfb41
Christiaan Bloemendaal <github@tnrd.net>
2025-09-06 17:57:12 +0200

    install + omarchy files

---
+++ install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Install basics
+sudo pacman -S --needed base-devel \
+			git \
+			nano
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -r yay
+
+# Install Hyprland + required
+yay -S --needed \
+			alacritty \
+			cliphist \
+			dunst \
+			eww \
+			hyprcursor \
+			hyprgraphics \
+			hypridle \
+			hyprland \
+			hyprpaper \
+			hyprpicker \
+			hyprpolkitagent \
+			hyprshot \
+			hyprsunset \
+			nautilus \
+			nautilus-admin-gtk4 \
+			nautilus-open-any-terminal \
+			nerd-fonts-git \
+			noto-fonts \
+			pipewire \
+			qt5-wayland \
+			qt6-wayland \
+			stow \
+			swaylock \
+			xdg-desktop-portal-hyprland
+
+# Install extra stuff
+yay -S --needed \
+			1password \
+			1password-cli \
+			bazaar \
+			btop \
+			cups \
+			cups-pdf \
+			discord \
+			dotnet-sdk \
+			dust \
+			eza \
+			flatpak \
+			gum \
+			jdk-openjdk \
+			jetbrains-toolbox \
+			lazygit \
+			less \
+			libreoffice-fresh \
+			localsend \
+			mission-center \
+			mono \
+			nextcloud-client \
+			nvtop \
+			openssh \
+			partitionmanager \
+			proton-pass-bin \
+			protonplus \
+			protontricks \
+			rsync \
+			steam \
+			tmux \
+			unityhub \
+			unzip \
+			visual-studio-code-bin \
+			vivaldi \
+			vivaldi-ffmpeg-codecs \
+			wine \
+			winetricks \
+			yazi \
+			zip
