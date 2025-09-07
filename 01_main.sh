6b73768ad1c63fbc6ca9a5428696c62820249cf7
= <github@tnrd.net>
2025-09-07 14:35:04 +0200

    typo

--- 01_main.sh
+++ 01_main.sh
@@ -7,7 +7,7 @@ source 02_basics.sh
 source 03_yay.sh
 source 04_rustup.sh
 source 05_hyprland.sh
-source 06_extra.sh
+source 06_extras.sh
 source 07_cleanup.sh
 source 08_export_bin.sh
 source 09_stow.sh
